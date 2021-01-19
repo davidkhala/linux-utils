fd392c763e1c5c5b8601f31fdf9262c1b2a25dbe
David Liu <david.yx.liu@oracle.com>
2021-01-19 22:03:56 +0800

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+prepare() {
+    email=${email:-"david-khala@hotmail.com"}
+    name=${name:-"david liu"}
+    git config --global user.email $email
+    git config --global user.name $name
+}
+switchBranch() {
+    local branchName=$1
+    echo "switching to branch origin/$branchName"
+    git checkout -b $branchName
+    git fetch
+    git branch --set-upstream-to=origin/$branchName $branchName
+    git pull
+}
+$@
