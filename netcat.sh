3b9c62a41d54d7f0ef47e50bac0be3bcfe9d5107
David Liu <david.yx.liu@oracle.com>
2021-02-07 22:45:42 -0500

    Update netcat.sh

--- netcat.sh
+++ netcat.sh
@@ -1,3 +1,10 @@
 # TODO powerful than nmap
+# Reference: https://blog.gtwang.org/linux/linux-utility-netcat-examples/
 # $ nc - arbitrary TCP and UDP connections and listens
 
+ping(){
+  local host=$1
+  local port=$2
+  nc -v $host $port
+}
+$@
