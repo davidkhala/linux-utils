29da0ca458de0471297a14967ef06ecd3da6eee1
David Liu <david.yx.liu@oracle.com>
2021-03-20 15:32:31 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
+## Inspired by [chainlink:using-docker](https://docs.chain.link/docs/running-a-chainlink-node#using-docker)
 install(){
   ## generic installer for linux 
   curl -sSL https://get.docker.com/ | sh
