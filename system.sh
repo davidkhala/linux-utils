6210d1ed42e89b99734efb26288d58186d0fd96b
David Liu <david.yx.liu@oracle.com>
2022-02-04 02:46:55 -0500

    Update system.sh

--- system.sh
+++ system.sh
@@ -14,6 +14,16 @@ viewRelease() {
 command_exists() {
 	command -v "$@" >/dev/null 2>&1
 }
+get_distribution() {
+	lsb_dist=""
+	# Every system that we officially support has /etc/os-release
+	if [ -r /etc/os-release ]; then
+		lsb_dist="$(. /etc/os-release && echo "$ID")"
+	fi
+	# Returning an empty string here should be alright since the
+	# case statements don't act unless you provide an actual value
+	echo "$lsb_dist"
+}
 # Check if this is a forked Linux distro
 check_forked() {
 
