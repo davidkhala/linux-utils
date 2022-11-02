67f7e76223965ea148c05ef9950564531bda0b51
David Liu <david.yx.liu@oracle.com>
2022-11-02 20:29:33 +0800

    Update editors.sh

--- editors.sh
+++ editors.sh
@@ -1,7 +1,10 @@
-## util for sed, awk, grep, tee or even vim
+## util for sed, awk, grep, tee, vim, xclip
 configure() {
   local configline=$1
   local file=$2
   grep -qF -- "$configline" "$file" || echo "$configline" >>"$file"
+}
+clip() {
+  xclip -sel clip $1
 }
 $@
