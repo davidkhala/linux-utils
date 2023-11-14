ab819b82b7b52e89672717e2b278bee831422cab
David Liu <david-khala@hotmail.com>
2023-11-14 19:38:10 +0800

    Update docker.sh

--- apps/docker.sh
+++ apps/docker.sh
@@ -2,20 +2,23 @@
 diagnose() {
 	ls -al /var/run/docker.sock
 }
-system-service() {
-	# On Debian and Ubuntu, the Docker service is configured to start on boot by default. 
- 	sudo systemctl enable --now containerd.service
-	sudo systemctl enable --now docker.service
-}
-
 ## Inspired by [chainlink:using-docker](https://docs.chain.link/docs/running-a-chainlink-node#using-docker)
 install() {
   ## generic installer for linux
   curl -sSL https://get.docker.com/ | sh
+  
+}
+rootless(){
+	
 
-  system-service
-  sudo usermod -aG docker $USER
-  # log in again
+}
+rootful(){
+	# On Debian and Ubuntu, the Docker service is configured to start on boot by default. 
+ 	sudo systemctl enable --now containerd.service
+	sudo systemctl enable --now docker.service
+ 
+  	sudo usermod -aG docker $USER
+  	# log in again
 }
 
 $@
