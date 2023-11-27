d154bec102cd65201d53643daa13c6a761e13822
David Liu <david-khala@hotmail.com>
2023-11-27 09:42:43 +0800

    Update docker.sh

--- apps/docker.sh
+++ apps/docker.sh
@@ -9,20 +9,32 @@ install() {
   
 }
 install-rootless(){
+  # NOTE PLEASE run this without 'sudo', otherwise ENV $USER will be 'root' instead of current user.
+  # See in https://docs.docker.com/engine/security/rootless/
 	curl -sSL https://get.docker.com/rootless | sh
-
+	export DOCKER_HOST=unix:///run/user/$UID/docker.sock
 }
 rootless(){
 	dockerd-rootless-setuptool.sh install
 
 }
 rootful(){
-	# On Debian and Ubuntu, the Docker service is configured to start on boot by default. 
- 	sudo systemctl enable --now containerd.service
-	sudo systemctl enable --now docker.service
+	# uninstall rootless
+	  if dockerd-rootless-setuptool.sh check ; then
+	  	dockerd-rootless-setuptool.sh uninstall
+	  fi
+	  
+	  # On Debian and Ubuntu, the Docker service is configured to start on boot by default. 
+ 	sudo systemctl enable --now containerd.service # optional
+	sudo systemctl enable --now docker.service # optional
+	  sudo groupadd docker
+	  sudo usermod -aG docker $USER
+   	echo Please open a new shell to see the effect...
+	  newgrp docker
+	 
  
-  	sudo usermod -aG docker $USER
-  	# log in again
+  	
+  	
 }
 
 $@
