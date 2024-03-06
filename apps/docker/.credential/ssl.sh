a0ced07a6c249d388983af7b8bb0f40b8c550e2b
David.Liu <david-khala@hotmail.com>
2024-03-06 20:44:52 +0800

    verified

--- apps/docker/.credential/ssl.sh
+++ apps/docker/.credential/ssl.sh
@@ -10,6 +10,16 @@ server() {
     openssl genrsa -out server-key.pem 4096
     openssl req -subj "/CN=$HOST" -sha256 -new -key server-key.pem -out server.csr
 }
+client() {
+    # create a client key and certificate signing request
+    openssl genrsa -out key.pem 4096
+
+    openssl req -subj '/CN=client' -new -key key.pem -out client.csr
+
+    echo extendedKeyUsage = clientAuth > extfile-client.cnf
+
+    openssl x509 -req -days 365 -sha256 -in client.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out cert.pem -extfile extfile-client.cnf
+}
 sign() {
     # sign the public key with our CA:
     echo subjectAltName = DNS:$HOST,IP:0.0.0.0,IP:127.0.0.1 >>extfile.cnf
@@ -18,7 +28,7 @@ sign() {
     openssl x509 -req -days 365 -sha256 -in server.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile extfile.cnf
 }
 clean() {
-    rm -v server.csr extfile.cnf
+    rm -v client.csr server.csr extfile.cnf extfile-client.cnf
 }
 run() {
     sudo dockerd --tlsverify -H=0.0.0.0:2376 \
