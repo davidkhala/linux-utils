f12ce324e7aa946e3dd750c36c197fe54436078a
David Liu <david-khala@hotmail.com>
2024-04-12 10:40:02 +0800

    Update editors.sh

--- editors.sh
+++ editors.sh
@@ -2,12 +2,12 @@
 append(){
   local content=$1
   local file=$2
-  echo $1 | sudo tee -a $file
+  echo $content | sudo tee -a $file
 }
 configure() {
   local configline=$1
   local file=$2
-  grep -qF -- "$configline" "$file" || echo "$configline" >>"$file"
+  grep -qF -- "$configline" "$file" || append "$configline" "$file"
 }
 clip() {
   xclip -sel clip $1
