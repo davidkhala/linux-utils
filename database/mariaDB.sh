332eb06fbe3da2cafe151926d180f388782cc3f2
David Liu <david-khala@hotmail.com>
2024-04-16 17:43:49 +0800

    Update mariaDB.sh

--- database/mariaDB.sh
+++ database/mariaDB.sh
@@ -1,6 +1,6 @@
 set -e
 post-install() {
     sudo systemctl enable --now mariadb.service
-    sudo /usr/bin/mysql_secure_installation
+    sudo /usr/bin/mysql_secure_installation # interactive wizard
 }
 $@
