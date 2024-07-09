d526ce2cc817e60ac8c106e0f30a304586f3e57c
David Liu <david-khala@hotmail.com>
2024-07-09 14:52:01 +0800

    Update protoc.sh

--- build-tools/protoc.sh
+++ build-tools/protoc.sh
@@ -1,12 +1,21 @@
 set -e
 version=${version:-25.1}
 
-install() {
+user-install(){
+    _install $HOME/.local
+
+}
+_install(){
     PB_REL="https://github.com/protocolbuffers/protobuf/releases"
     FILENAME=protoc-${version}-linux-$(uname -p).zip
     curl -LO $PB_REL/download/v${version}/${FILENAME}
-    unzip $FILENAME -d /usr/local
+    unzip $FILENAME -d $1
     rm $FILENAME
+    
+}
+system-install() {
+    _install /usr/local
+    
 }
 version() {
     protoc --version
