4bfb41dbc1c6357935763e7bc42fe07559875e68
David Liu <david-khala@hotmail.com>
2024-07-22 09:38:38 +0800

    Update usr.sh

--- system/usr.sh
+++ system/usr.sh
@@ -5,6 +5,12 @@ prepare-home-dir(){
 reset-password() {
     sudo passwd $1
 }
+reset-root-password(){
+  reset-password
+}
+reset-current-password(){
+  reset-password $USER
+}
 create(){
   sudo useradd -m $1
 }
