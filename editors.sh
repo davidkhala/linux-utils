b6ddb39254d097b864abf8a5d14dc8f258c7c0e0
David Liu <david-khala@hotmail.com>
2024-10-20 09:45:34 +0000

    enhance with inline replace

--- editors.sh
+++ editors.sh
@@ -1,13 +1,33 @@
 ## util for sed, awk, grep, tee, vim, xclip
-append(){
+set -e
+append() {
   local content=$1
   local file=$2
-  echo $content | sudo tee -a $file
+  local inline=$3
+  if [[ -z $inline ]]; then
+    # default append to newline
+    sudo sed -i "\$a\\$content" $file
+  else
+    echo $content | sudo tee -a $file
+  fi
+
 }
 configure() {
   local configline=$1
   local file=$2
-  grep -qF -- "$configline" "$file" || append "$configline" "$file"
+  read key value <<<$(echo "$configline" | awk -F= '{print $1,$2}')
+
+  if [[ -z $value ]]; then
+    echo "value is empty"
+    return 1
+  fi
+
+  if grep --quiet "^$key=.*" $file; then
+    sed --in-place --regexp-extended "s/^$key=.*/$key=$value/" $file
+  else
+    append "$key=$value" $file
+  fi
+
 }
 clip() {
   xclip -sel clip $1
--- test/bash.sh
+++ test/bash.sh
@@ -1,4 +1,10 @@
 printRest() {
     echo "$@"
 }
+testAppend() {
+    ../editors.sh append newkey=value .env
+}
+config() {
+    ../editors.sh configure key=value ./.env
+}
 $@
