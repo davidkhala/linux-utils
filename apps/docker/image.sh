2db848ff353a40dda44f774f49d7571568287995
David Liu <david-khala@hotmail.com>
2025-11-20 08:39:39 +0800

    Update image.sh

--- apps/docker/image.sh
+++ apps/docker/image.sh
@@ -6,9 +6,32 @@ load() {
 	docker image load --input $1
 
 }
-trim() {
+prune() {
 	echo WARNING! This will remove all images without at least one container associated to them.
+	sleep 5
 	docker image prune -a
+}
+trim(){
+	# keep only the latest images
+	docker images --format "{{.Repository}} {{.Tag}} {{.ID}} {{.CreatedAt}}" \
+	  | sort -k1,1 -k4,4r \
+	  | awk '
+	    {
+	      repo=$1;
+	      tag=$2;
+	      id=$3;
+	      if (!seen[repo]++) {
+	        # 第一次出现的镜像（最新），保留
+	        print "保留: " repo ":" tag " (" id ")";
+	      } else {
+	        # 其他的删除
+	        print id;
+	      }
+	    }
+	  ' \
+	  | grep -v "^保留" \
+	  | xargs -r docker rmi
+
 }
 
 build() {
